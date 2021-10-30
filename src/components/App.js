bf66aa01782695c141fa4855081ffdb47635963a
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-10-31 02:06:31 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import '../App.css';
+import Percent from './Percent';
 
 function App() {
-  return <div className='App'>фвцф</div>;
+  return (
+    <div className='App'>
+      <Percent></Percent>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Percent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Percent() {
+	const input = 
+  return (
+    <div className='percent'>
+      <input placeholder='Введите вес сырья'></input>
+      <input type='checkbox'></input>
+      <input type='checkbox'></input>
+      <button className='count-btn'>Рассчитать</button>
+    </div>
+  );
+}
+
+export default Percent;
