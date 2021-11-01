8d089bb4aa9fe17a2ba075fc0b9c8e09c5aa38d1
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-11-02 00:08:14 +0300

    im stuck

--- src/components/Percent.js
+++ src/components/Percent.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import '../Percent.css';
 
 function Percent() {
-	const input = 
+  // const input =
+  const countPercent = () => {
+    console.log('click' + 1);
+  };
+
   return (
-    <div className='percent'>
-      <input placeholder='Введите вес сырья'></input>
-      <input type='checkbox'></input>
-      <input type='checkbox'></input>
-      <button className='count-btn'>Рассчитать</button>
+    <div className='wrap-percent'>
+      <div className='percent'>
+        <h1>Рассчитать процент красителя</h1>
+        <section className='input'>
+          <input placeholder='Введите вес сырья'></input>
+        </section>
+        <section className='check'>
+          <input type='checkbox'></input>
+          <span>2%</span>
+          <input type='checkbox'></input>
+          <span>1%</span>
+        </section>
+        <button onClick={countPercent} className='count-btn'>
+          Рассчитать
+        </button>
+      </div>
     </div>
   );
 }
